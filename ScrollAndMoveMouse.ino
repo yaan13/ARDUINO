6ca35de1da3e5639d7bbca6e51ea698d7b862535
yaan13 <100844627+yaan13@users.noreply.github.com>
2022-06-16 19:37:44 +0800

    Add files via upload

---
+++ ScrollAndMoveMouse.ino
@@ -0,0 +1,130 @@
+#include "I2Cdev.h"
+#include <Wire.h>
+#include <MPU6050.h>
+#include <BleMouse.h>
+#include "Kalman.h"
+
+#define RESTRICT_PITCH
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+
+BleMouse bleMouse;
+MPU6050 imu;
+int16_t ax, ay, az, gx, gy, gz;
+int16_t accX, accY, accZ;
+int16_t gyroX, gyroY, gyroZ;
+int vx, vy;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+uint32_t timer;
+uint8_t i2cData[14];
+
+int buttonL = 0; // IO0 button
+int buttonR = 4;
+int buttonLstate = HIGH; 
+int buttonRstate = HIGH; 
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  pinMode(buttonL, INPUT_PULLUP);
+  pinMode(buttonR, INPUT_PULLUP);
+  
+  Serial.print("MPU6050 initializing");
+  imu.initialize();
+  while (!imu.testConnection()) { Serial.print("."); }
+  Serial.println();  
+  Serial.println("BLE Mouse starting !");
+  bleMouse.begin();
+
+  imu.getAcceleration(&accX, &accY, &accZ);
+  
+ #ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+
+  timer = micros();
+}
+
+void loop() {
+  imu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  imu.getAcceleration(&accX, &accY, &accZ);
+  imu.getRotation(&gyroX, &gyroY, &gyroZ);
+
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+  
+#ifdef RESTRICT_PITCH
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+#else
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+    kalmanY.setAngle(pitch);
+    compAngleY = pitch;
+    kalAngleY = pitch;
+    gyroYangle = pitch;
+  } else
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+
+if (abs(kalAngleY) > 90)
+  gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+#endif
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+  
+  bleMouse.move(compAngleX , compAngleY );
+    
+  buttonLstate = digitalRead(buttonL);
+  buttonRstate = digitalRead(buttonR);  
+  
+  if (buttonLstate == LOW) { // press button to Ground
+    bleMouse.click(MOUSE_LEFT);
+    delay(100);
+  } 
+  else if(buttonRstate == LOW) { // press button to Ground
+    bleMouse.click(MOUSE_RIGHT);
+    delay(100);
+  }
+  delay(100);
+}
